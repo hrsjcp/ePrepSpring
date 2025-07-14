a91626ea4e41c4b004827dae7432612639da7a7b
Harsh Patel <52949898+imhrsjp@users.noreply.github.com>
2025-07-14 13:32:49 +0530

    EP-03

--- src/main/java/com/hrsjp/ePrepSpring/controller/UserController.java
+++ src/main/java/com/hrsjp/ePrepSpring/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.hrsjp.ePrepSpring.controller;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hrsjp.ePrepSpring.entities.*;
